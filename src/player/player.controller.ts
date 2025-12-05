e71220760d686b4c20f77b587adb7be54996e68b
Sven Toye <sven.toye@twofold.swiss>
2025-12-05 15:30:11 +0100

    Add auth guards

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -6,31 +6,37 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from "@nestjs/common";
 import { PlayerService } from "./player.service";
 import { CreatePlayerDto } from "./dto/create-player.dto";
 import { UpdatePlayerDto } from "./dto/update-player.dto";
 import { Player } from "./entities/player.entity";
+import { JwtGuard } from "src/auth/jwt.guard";
 
 @Controller("player")
 export class PlayerController {
   constructor(private readonly playerService: PlayerService) {}
 
+  @UseGuards(JwtGuard)
   @Post()
   create(@Body() createPlayerDto: CreatePlayerDto): Promise<Player> {
     return this.playerService.create(createPlayerDto);
   }
 
+  @UseGuards(JwtGuard)
   @Get()
   findAll(): Promise<Player[]> {
     return this.playerService.findAll();
   }
 
+  @UseGuards(JwtGuard)
   @Get(":id")
   findOne(@Param("id") id: number): Promise<Player | null> {
     return this.playerService.findOne(id);
   }
 
+  @UseGuards(JwtGuard)
   @Patch(":id")
   update(
     @Param("id") id: number,
@@ -39,6 +45,7 @@ export class PlayerController {
     return this.playerService.update(id, updatePlayerDto);
   }
 
+  @UseGuards(JwtGuard)
   @Delete(":id")
   remove(@Param("id") id: number): Promise<void> {
     return this.playerService.remove(id);
