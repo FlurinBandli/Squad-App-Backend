0722a7f48e2eda20f72402433c8f61cae013702b
Sven Toye <sven.toye@twofold.swiss>
2025-12-05 11:18:09 +0100

    Add swagger endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,16 @@ async function bootstrap() {
       },
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle("Teamkader App")
+    .setDescription(
+      "Der Fussballverein des VfB Zürich-Leutschenbach verschickt zu jedem Spieltag seiner Jugendmannschaften eine Email an die Spieler/-innen und deren Eltern, in der der jeweilige Mannschaftskader bekannt gegeben wird.",
+    )
+    .setVersion("1.0")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
