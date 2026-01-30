a4a17e0d4fad1a2a06c6ba584d0f8f25bfc31b8f
Sven Toye <sven.toye@twofold.swiss>
2026-01-30 08:26:21 +0100

    Improve nix derivation
    Fetch source code through git repo with hash so that it is reproducable

--- default.nix
+++ default.nix
@@ -2,8 +2,11 @@
   pkgs ? import <nixpkgs> { },
 }:
 pkgs.buildNpmPackage {
-  src = ./.;
   pname = "squad-app-backend-pipa";
   version = "0.0.1";
+  src = fetchGit {
+    url = ./.;
+    rev = "8b26e13e2c4c247015ec7e4bbd1ce34dc051609f";
+  };
   npmDepsHash = "sha256-5Noq2GrLiRXWa7oaGYTWC788vcJjvOHfONdrUFW7VKA=";
 }
